23a033eb2d26d803cab92141ef4f97ca37c2284a
jjmansilla <jjmansilla@gmail.com>
2014-02-04 08:33:44 +0100

    1: Escribir Nombre

--- src/github/Github.java
+++ src/github/Github.java
@@ -13,7 +13,8 @@ public class Github {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) 
+    {
+        System.out.println("Julio");
     }
 }
